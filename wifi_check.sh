0a768806db3520cd4ad11f85e3573d9e7bc1a066
Eco Innovate <birdboxers11124@gmail.com>
2024-08-19 18:43:46 -0700

    Update wifi_check.sh

--- wifi_check.sh
+++ wifi_check.sh
@@ -5,13 +5,16 @@
 
  if [ $? -ne 0 ]; then
 
- # Wi-Fi is not connected, send an email notification (optional) echo
+ # Wi-Fi is not connected, send us an email (we are paranoid.....)
 
 mail -s "Wi-Fi Connection Issue" birdboxers11124@gmail.com
 
- # Shut down Raspberry Pi
+ # Shut down Raspberry Pi (its for your own good :_()
 
 sudo shutdown -h now
  else
 echo "Wi-Fi is connected."
  fi
+
+#HELLO IF YOURE READING THIS HAHAH!! this script specifically may need some adjustments
+#its a little counterintuitive rn; if the pi cant even connect to googles server, how would it have the internet to email us??
